05ebc5d5f061b0db913613ae3a394cd05b1123a9
Nicolas Deman <nicolas.deman@hotmail.fr>
2024-02-13 14:38:37 +0100

    add better sql lite

--- commands/testing/ping.js
+++ commands/testing/ping.js
@@ -1,17 +1,20 @@
 const { SlashCommandBuilder } = require('discord.js');
-//const db = require('quick.db');
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Commande de test simple'),
-	execute(interaction) {
+	async execute(interaction) {
 		interaction.reply('Pong.');
 
-		/* Record number of command usage
-		db.add('times.ping', 1);
-		const timesUsed = db.get('times.ping');
-		interaction.channel.send('This command has been used '+timesUsed+' times!');
-		*/
+		//Record number of command usage
+		await db.add('times.ping', 1);
+		let timesUsed = 0;
+		await db.get('times.ping').then(timesUsed => {
+			this.timesUsed = timesUsed;
+		});
+		interaction.channel.send('This command has been used '+this.timesUsed+' times!');
 	},
 };
