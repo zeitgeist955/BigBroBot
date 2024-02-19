f7540b9d4e2157401a7fd60a49b42e57af9ebe0e
Nicolas Deman <nicolas.deman@hotmail.fr>
2024-02-19 16:22:45 +0100

    select a sound to play from a library file (as an enum)

--- commands/voice/play-soundboard.js
+++ commands/voice/play-soundboard.js
@@ -1,22 +1,32 @@
-const { getVoiceConnection, VoiceConnectionStatus, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+const { getVoiceConnection, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
 const { join } = require('node:path');
 const { SlashCommandBuilder } = require('discord.js');
+
+const soundLibrary = require('../../ressources/sound-library.js');
+const library = soundLibrary.LIBRARY;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('play-sounboard')
-        .setDescription('Joue un son de la soundboard du serveur'),
-        //TODO select which sound to play
+        .setDescription('Joue un son de la soundboard du serveur')
+        .addStringOption(option => 
+            option.setName('son')
+            .setDescription('Choix du son')
+            .setRequired(true)
+            .addChoices(...library) //Spread the array, limit to 25
+        ),
 
     async execute(interaction) {
         const connection = getVoiceConnection(interaction.guildId);
         const audioPlayer = createAudioPlayer();
 
-        let resource = createAudioResource(join(__dirname, '../../ressources/ronfle.mp3'));
+        const fileChoice = interaction.options.getString('son');
+
+        let resource = createAudioResource(join(__dirname, '../../ressources/'+fileChoice));
 
         const subscription = connection.subscribe(audioPlayer);
 
         audioPlayer.play(resource);
-        interaction.reply('Son joué');
+        interaction.reply('Son joué'); //TODO advert if the file isnt found or an error occurs
     },
 };
---
+++ ressources/sound-library.js
@@ -0,0 +1,5 @@
+exports.LIBRARY = [
+    { name: 'mec qui ronfle fort', value: 'ronfle.mp3'},
+    { name: 'test fichier inexistant', value: 'test.mp3'},
+    { name: 'test 2', value: 'test2.mp3'}
+];
